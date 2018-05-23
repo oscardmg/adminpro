b894e7e84d8b460a61624005288268fb2500e8c2
omontoya <omontoya@comodisimos.com>
2018-05-23 14:08:37 -0500

    Input, output y viewchild

--- src/app/components/incrementador/incrementador.component.ts
+++ src/app/components/incrementador/incrementador.component.ts
@@ -7,6 +7,7 @@ import { Component, Input, Output, ViewChild, OnInit, EventEmitter, ElementRef }
 })
 export class IncrementadorComponent implements OnInit {
 
+  //
   @ViewChild('txtProgress') txtProgress: ElementRef;
 
   @Input('nombre') leyenda: string = 'Leyenda';
